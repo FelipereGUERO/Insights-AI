ff6c6a4377dd2c4451bd4b761396fc3d0b8858c9
FelipereGUERO <felipereguero@gmail.com>
2026-06-30 19:30:24 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Insight AI",
+    page_icon="📊",
+    layout="wide"
+)
+
+st.title("📊 Insight AI")
+
+st.write("Bem-vindo ao Insight AI!")
+
+st.success("Nosso sistema está funcionando.")
